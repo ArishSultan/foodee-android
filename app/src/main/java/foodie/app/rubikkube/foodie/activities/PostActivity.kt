1a502a896e5138787faf724289bc15a35b5dcb58
ahmedshehryar <sa03158704640@gmail.com>
2019-05-14 16:46:01 +0500

    update

--- app/src/main/java/foodie/app/rubikkube/foodie/activities/PostActivity.kt
+++ app/src/main/java/foodie/app/rubikkube/foodie/activities/PostActivity.kt
@@ -3,11 +3,16 @@ package foodie.app.rubikkube.foodie.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import foodie.app.rubikkube.foodie.R
+import kotlinx.android.synthetic.main.activity_post.*
 
 class PostActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+
+        back_icon.setOnClickListener {
+            finish()
+        }
     }
 }
